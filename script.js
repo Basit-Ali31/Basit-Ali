7d08588baac3189cb41406e6e685fc3e5e40c646
Basit-Ali31 <basitali1031@gmail.com>
2024-10-31 02:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle the navigation menu on mobile
+document.querySelector('#icon').addEventListener("click", function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+});
+
